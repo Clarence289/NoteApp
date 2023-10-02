34a1fb91a134314b9c91b2e918fb05ef98bfd6b3
TemoshoS <temoshomaduane@gmail.vom>
2023-10-02 16:11:27 +0200

    firabaseService

--- .history/components/Home_20231002161050.js
+++ .history/components/Home_20231002161050.js
@@ -4,28 +4,26 @@ import firebaseService from './firebaseService';
 
 
 const Home = () => {
-  const [title, setTitle] = useState('');
-  const [note, setNote] = useState('');
 
   return (
     <View style={styles.container}>
-    <Text>Adding Note Screen</Text>
-    <TextInput
-      style={styles.titleContainer}
-      placeholder="Title"
-      value={title}
-      onChangeText={(text) => setTitle(text)}
-    />
-    <TextInput
-      style={styles.noteContainer}
-      placeholder="Note"
-      value={note}
-      onChangeText={(text) => setNote(text)}
-    />
-    <TouchableOpacity style={styles.button} onPress={() => firebaseService.saveNote(title, note)}>
-      <Text style={styles.buttonText}>Save</Text>
-    </TouchableOpacity>
-  </View>
+      <Text>Adding Note Screen</Text>
+      <TextInput
+        style={styles.titleContainer}
+        placeholder="Title"
+        
+        onChangeText={(text) => setTitle(text)}
+      />
+      <TextInput
+        style={styles.noteContainer}
+        placeholder="Note"
+        
+        onChangeText={(text) => setNote(text)}
+      />
+      <TouchableOpacity style={styles.button} >
+        <Text style={styles.buttonText}>Save</Text>
+      </TouchableOpacity>
+    </View>
   );
 };
 
--- .history/components/Home_20231002161058.js
+++ .history/components/Home_20231002161058.js
@@ -9,23 +9,23 @@ const Home = () => {
 
   return (
     <View style={styles.container}>
-    <Text>Adding Note Screen</Text>
-    <TextInput
-      style={styles.titleContainer}
-      placeholder="Title"
-      value={title}
-      onChangeText={(text) => setTitle(text)}
-    />
-    <TextInput
-      style={styles.noteContainer}
-      placeholder="Note"
-      value={note}
-      onChangeText={(text) => setNote(text)}
-    />
-    <TouchableOpacity style={styles.button} onPress={() => firebaseService.saveNote(title, note)}>
-      <Text style={styles.buttonText}>Save</Text>
-    </TouchableOpacity>
-  </View>
+      <Text>Adding Note Screen</Text>
+      <TextInput
+        style={styles.titleContainer}
+        placeholder="Title"
+        
+        onChangeText={(text) => setTitle(text)}
+      />
+      <TextInput
+        style={styles.noteContainer}
+        placeholder="Note"
+        
+        onChangeText={(text) => setNote(text)}
+      />
+      <TouchableOpacity style={styles.button} >
+        <Text style={styles.buttonText}>Save</Text>
+      </TouchableOpacity>
+    </View>
   );
 };
 
