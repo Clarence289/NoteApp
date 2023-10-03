b14e646da153f0708820f95259773421435653a8
Lucky Cungwa <36919645+luckycungwa@users.noreply.github.com>
2023-10-03 09:19:11 +0300

    added function for retrieving notes from db

--- service/firebaseService.js
+++ service/firebaseService.js
@@ -1,43 +1,51 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { db } from '../config/firabase';
-import {  addDoc,collection } from 'firebase/firestore';
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { db } from "../config/firabase";
+import { addDoc, collection } from "firebase/firestore";
 
 export default function firebaseService() {
+  const [title, setTitle] = useState("");
+  const [note, setNote] = useState("");
 
-  const [title, setTitle]= useState('');
-  const [note, setNote] = useState('');
-
-  const saveNote = async()=>{
-    const notesCollectionRef =  collection(db, 'notes');
+  const saveNote = async () => {
+    const notesCollectionRef = collection(db, "notes");
 
     const noteData = {
-        title,
-        note,
-        timestamp: new Date().toISOString(),
-      }
+      title,
+      note,
+      timestamp: new Date().toISOString(),
+    };
 
     try {
-      
-
       const noteRef = await addDoc(notesCollectionRef, noteData);
 
-      setNote('');
-      setTitle('');
-      alert('Note saved successfully.');
-      
+      setNote("");
+      setTitle("");
+      alert("Note saved successfully.");
     } catch (error) {
-
-      alert('Error saving note:', error);
-      
-      
+      alert("Error saving note:", error);
     }
-  }
+  };
+
+  // get all the available notes from the db "notes" collection
+  const fetchNotes = async () => {
+    const notesCollectionRef = collection(db, "notes");
+    const querySnpshot = await getDocs(notesCollectionRef);
+
+    //  map throuh all the the notes async
+    const notes = [];
+    querySnpshot.array.forEach((doc) => {
+      const noteData = doc.data(); //get & make avaialble data from the document
+      notes.push(noteData);
+    });
+    return notes;
+  };
+
   return (
     <View>
       <Text>firebaseService</Text>
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
